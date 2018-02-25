fa1c3e5b470f21c2d5b702ac82e879fb38e863e7
saarikamylavarapu <saarikaj@gmail.com>
2018-02-25 21:47:51 +0000

    Subsequent Commit

--- src/main/java/utilities/AssertionUtilityFunctions.java
+++ src/main/java/utilities/AssertionUtilityFunctions.java
@@ -7,20 +7,12 @@ public class AssertionUtilityFunctions {
 
     public static void assertStringValue(String expected, String actual){
         Assert.assertEquals(expected, actual);
-       /*try {
-           Assert.assertEquals(expected, actual);
-       }catch (AssertionError ae){
-           ae.getMessage();
-       }*/
+
     }
 
     public static void assertStringContains(String expected, String actual){
 
         Assert.assertTrue(actual.contains(expected));
-       /* try {
-            Assert.assertTrue(actual.contains(expected));
-        }catch (AssertionError ae){
-            ae.getMessage();
-        }*/
+
     }
 }
